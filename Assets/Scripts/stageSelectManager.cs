396802d801e43d266f9537fdd6cc625794e35bce
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-08-09 09:52:05 +0900

    StartScene 초기 stage 값 1로 설정 및 게임 Scene의 timeText Width 조정

--- Assets/Scripts/stageSelectManager.cs
+++ Assets/Scripts/stageSelectManager.cs
@@ -16,7 +16,7 @@ public class stageSelectManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        SSM.stage = 1;
     }
 
     // Update is called once per frame
